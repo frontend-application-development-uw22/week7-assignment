9a96097856c5a68bc1e7e57a183119f892022f36
ambz <a.bandasith@gmail.com>
2022-06-16 15:03:07 -0700

    adding src files

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './components/styles/Global';
+import NavLinks from './components/Routes';
+import Footer from './components/Footer'
+
+const theme = {
+  colors: {
+    header: '#cdd3c2',
+    body: '#fff',
+    footer: '#e4c1a0',
+  },
+
+  mobile: '768px',
+}
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <NavLinks />
+        <Footer />
+      </>
+    </ThemeProvider>
   );
 }
 
